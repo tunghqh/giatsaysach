0c223d98f286cb07894f9ad801213a05f0725fec
tunghqh <tungcules1999@gmail.com>
2025-10-23 10:21:49 +0700

    change qr code

--- app/helpers/qr_helper.rb
+++ app/helpers/qr_helper.rb
@@ -3,7 +3,7 @@ module QrHelper
   # Tham khảo: https://vietqr.net/documentation.html
   # Chỉ cần account, bank_id, amount, addInfo
   def vietqr_payload_vpbank(account_number:, amount:, add_info: '')
-    bank_id = '970432' # VPBank
+    bank_id = '970407' # Techcombank
     # Sử dụng link động của vietqr.net để lấy ảnh QR
     "https://img.vietqr.io/image/#{bank_id}-#{account_number}-compact.png?amount=#{amount}&addInfo=#{CGI.escape(add_info)}"
   end
