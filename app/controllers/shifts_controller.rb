8f4ff9e1080d9f9e8fe0468db87d5417e8f6f4ae
tunghqh <tungcules1999@gmail.com>
2026-03-11 08:53:14 +0700

    add shift name

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -5,7 +5,7 @@ class ShiftsController < ApplicationController
 
   # Staff screen: list recent shifts and start new one
   def index
-    @staff_names = ['Lâm', 'Vy', 'Tâm', 'Trang', 'Diễm']
+    @staff_names = ['Lâm', 'Vy', 'Tâm', 'Trang', 'Diễm', 'Hân']
     @selected_staff = params[:staff_name] || @staff_names.first
     @shifts = Shift.for_staff(@selected_staff).order(created_at: :desc).limit(20)
     @shift = Shift.new(staff_name: @selected_staff)
@@ -22,7 +22,7 @@ class ShiftsController < ApplicationController
     if @shift.save
       redirect_to shifts_path(staff_name: @shift.staff_name), notice: 'Đã bắt đầu ca.'
     else
-      @staff_names = ['Lâm', 'Vy', 'Dũng', 'Tâm', 'Diễm']
+      @staff_names = ['Lâm', 'Vy', 'Tâm', 'Trang', 'Diễm', 'Hân']
       @selected_staff = params[:staff_name] || @staff_names.first
       @shifts = Shift.for_staff(@selected_staff).order(created_at: :desc).limit(20)
       @shift = Shift.new(staff_name: @selected_staff)
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,6 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 # Create admin user
-if Rails.env.production?
-  admin_email = "admin@gmail.com"
-  admin_password = "123456"
-
-  admin_user = User.find_or_create_by(email: admin_email) do |user|
-    user.password = admin_password
-    user.password_confirmation = admin_password
-  end
-
-  puts "Admin user created: #{admin_user.email}" if admin_user.persisted?
-end
 
 # Example:
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
